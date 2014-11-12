f7d1fc6fe2ab6c44afa73d4f87fa72a6b0517a77
Thibaud Michaud <michau_n@epita.fr>
2014-11-12 01:49:32 +0100

    Code: plot error with and without gradient descent on separate figs.

--- code/plot_error_logreg.py
+++ code/plot_error_logreg.py
@@ -23,7 +23,7 @@ def plot_error(t, ax):
         for i in range(len(w0)):
             w = transpose(matrix([w0[i], w1[j]]))
             Z[j, i] = error_tot(t, w)
-    ax.plot_surface(W0, W1, Z, cstride=1, rstride=1, cmap=plt.cm.jet, alpha = 0.5)
+    return W0, W1, Z
 
 def plot_learning_path(t, ax):
     # Start with some arbitrary weights.
@@ -32,17 +32,17 @@ def plot_learning_path(t, ax):
     pathx = [w[0, 0]]
     pathy = [w[1, 0]]
     pathz = [error_tot(t, w)[0, 0]]
-    for i in range(1, 21):
+    for i in range(1, 31):
         for x, y in t:
             w = learn(x, y, w, 0.3)
         pathx.append(w[0, 0])
         pathy.append(w[1, 0])
         pathz.append(error_tot(t, w)[0, 0])
-    ax.plot(pathx, pathy, pathz, color='r', marker='o')
+    return pathx, pathy, pathz
 
 # Training set: a list of pair (input, target).
 t = []
-for i in range(20):
+for i in range(10):
     # Input is a random number between -0.5 and 0.5.
     x = [random.random() - 0.5]
 
@@ -58,11 +58,24 @@ t = [(matrix([1] + x), y) for x, y in t]
 # inputs.
 w = random.rand(t[0][0].size, 1)
 
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
+fig1 = plt.figure(0)
+ax = fig1.add_subplot(111, projection='3d')
 ax.set_xlabel('w0')
 ax.set_ylabel('w1')
 ax.set_zlabel('error')
-plot_error(t, ax)
-plot_learning_path(t, ax)
+W0, W1, Z = plot_error(t, ax)
+ax.plot_surface(W0, W1, Z, cstride=1, rstride=1, cmap=plt.cm.jet, alpha = 0.5)
+ax.view_init(30, 60)
+plt.tight_layout()
+
+fig2 = plt.figure(1)
+ax2 = fig2.add_subplot(111, projection='3d')
+ax2.plot_surface(W0, W1, Z, cstride=1, rstride=1, cmap=plt.cm.jet, alpha = 0.5)
+ax2.set_xlabel('w0')
+ax2.set_ylabel('w1')
+ax2.set_zlabel('error')
+pathx, pathy, pathz = plot_learning_path(t, ax2)
+ax2.plot(pathx, pathy, pathz, color='r', marker='o')
+ax2.view_init(30, 60)
+plt.tight_layout()
 plt.show(block=True)
